b493ac9870cfa07c6c063fe49d3c22562184246e
Anup Kumar <anup.raj1989@gmail.com>
2020-11-20 19:56:52 +0530

    feat(micro-frontend): add the layout component

---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'profile-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent {
+  title = 'anupbaranwal';
+  constructor() { }
+
+}
