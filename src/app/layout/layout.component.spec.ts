e53e269330a174264ed3030502263cb4d3c366e7
Anup Kumar <anup.raj1989@gmail.com>
2020-11-20 20:37:53 +0530

    fix(lint): Shadowed name fixture

--- src/app/layout/layout.component.spec.ts
+++ src/app/layout/layout.component.spec.ts
@@ -24,14 +24,10 @@ describe('LayoutComponent', () => {
   });
 
   it(`should have as title 'anupbaranwal'`, () => {
-    const fixture = TestBed.createComponent(LayoutComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('anupbaranwal');
+    expect(component.title).toEqual('anupbaranwal');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(LayoutComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('anupbaranwal app is running!');
   });
